56b8e8c921f2de068c99fd313c212ea6ce2c2482
Suzie K <suziekim.dev@gmail.com>
2021-02-23 22:03:19 -0500

    initial commit, add two files

---
+++ variable_practice.rb
@@ -0,0 +1,10 @@
+str = "here's a clear example of string!"
+password = "8890876x``!"
+cat_age = 4
+all_cat_age = [2, 3, 5, 10]
+is_hungry = true
+temp_in_F = 86.2
+
+str = "is this a reassignment?"
+temp_in_F = 62.4
+cat_age = 12
